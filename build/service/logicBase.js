6569d453076abaa1f24f40d8a085fb9fc3273246
AyyasyRosadi <ayyasyrsd@gmail.com>
2024-05-26 17:52:03 +0800

    first commit

--- build/service/logicBase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LogicBase = void 0;
-class LogicBase {
-    message(status, data) {
-        return { data: data, status: status };
-    }
-}
-exports.LogicBase = LogicBase;
